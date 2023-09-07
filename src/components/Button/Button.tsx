2c880e8e508cbb602030466a14837deab739a9cc
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-07 22:23:12 +0400

    feat: add ui button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import cn from 'classnames';
+
+import Text from 'components/Text';
+import Loader from 'components/Loader';
+import styles from './Button.module.scss';
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+  children: React.ReactNode;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  loading,
+  disabled,
+  className,
+  ...nativeProps
+}) => {
+  return (
+    <button
+      type="button"
+      {...nativeProps}
+      disabled={disabled === true || loading === true}
+      className={cn(
+        className,
+        styles.button,
+        disabled && styles.button_disabled
+      )}
+    >
+      {loading && <Loader size="s" className={styles.button_loader} />}
+      <Text className="button_text">{children}</Text>
+    </button>
+  );
+};
+
+export default Button;
