3bd9d6324b6e7b47257ef0bc06363ff91374e52c
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-10 14:45:55 +0400

    refactor: change page appearance

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,14 @@
-function NotFoundPage() {
-  return <div>NotFoundPage</div>;
-}
+import Text from 'components/Text';
+import styles from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => {
+  return (
+    <main className={styles.notFound}>
+      <Text view="title" tag="h2">
+        Sorry, there are no such a page
+      </Text>
+    </main>
+  );
+};
 
 export default NotFoundPage;
