d6e88ad9cb0e56af73d3658c8de5209ff1df617f
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-10 13:39:29 +0400

    refactor: add constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,12 @@
-export const headerLinks = [
+const baseURL = 'https://mock-server-api-kazakovmaksim.vercel.app';
+const catalog = `${baseURL}/api/catalog`;
+
+const cardsByPage = 9;
+
+const headerLinks = [
   { name: 'Products', path: '/' },
   { name: 'Categories', path: '*' },
   { name: 'About us', path: '/about' },
 ];
+
+export { headerLinks, baseURL, catalog, cardsByPage };
