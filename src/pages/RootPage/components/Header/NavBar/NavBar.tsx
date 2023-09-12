1012569095ee89078f2ba03b41e1edf2ee681110
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-12 09:17:55 +0400

    refactor: move 'setActive' outside component

--- src/pages/RootPage/components/Header/NavBar/NavBar.tsx
+++ src/pages/RootPage/components/Header/NavBar/NavBar.tsx
@@ -3,10 +3,10 @@ import { headerLinks } from 'constants/index';
 import Text from 'components/Text';
 import styles from './NavBar.module.scss';
 
-const NavBar = () => {
-  const setActive = ({ isActive }: { isActive: boolean }) =>
-    isActive ? styles.active : '';
+const setActive = ({ isActive }: { isActive: boolean }) =>
+  isActive ? styles.active : '';
 
+const NavBar = () => {
   return (
     <ul className={styles.nav}>
       {headerLinks.map((link) => (
