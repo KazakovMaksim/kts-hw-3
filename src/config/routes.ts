1089156ddaa03f19611c16c0b0f3079df97fe825
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-12 09:24:51 +0400

    refactor: use ROUTES in headerLinks

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,7 @@
 const ROUTES = {
   MAIN: '/',
+  ABOUT: '/about',
+  CATEGORIES: '/categories',
   PRODUCT: '/product/:productId',
   REGISTER: '/auth/registration',
   LOGIN: '/auth/login',
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,14 @@
+import ROUTES from 'config/routes';
+
 const baseURL = 'https://mock-server-api-kazakovmaksim.vercel.app';
 const catalog = `${baseURL}/api/catalog`;
 
 const cardsByPage = 9;
 
 const headerLinks = [
-  { name: 'Products', path: '/' },
-  { name: 'Categories', path: '*' },
-  { name: 'About us', path: '/about' },
+  { name: 'Products', path: ROUTES.MAIN },
+  { name: 'Categories', path: ROUTES.CATEGORIES },
+  { name: 'About us', path: ROUTES.ABOUT },
 ];
 
 export { headerLinks, baseURL, catalog, cardsByPage };
