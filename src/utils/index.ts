492baecc3f8607f47be2642b2d11d2679b8cac21
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-15 20:14:21 +0400

    feat: add getErrorMessage func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,23 @@
 import { API_ENDPOINTS } from 'config/api';
 import { BASE_URL } from 'constants/index';
+import { isRouteErrorResponse } from 'react-router-dom';
 
 export const getProductsURL = (page: number, limit: number) => {
   return `${BASE_URL}${API_ENDPOINTS.PRODUCTS}/?offset=${page}&limit=${limit}`;
 };
+
+export function getErrorMessage(error: unknown) {
+  if (isRouteErrorResponse(error)) {
+    return `${error.status} ${error.statusText}`;
+  }
+
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (typeof error === 'string') {
+    return error;
+  }
+
+  return 'Unknown error';
+}
