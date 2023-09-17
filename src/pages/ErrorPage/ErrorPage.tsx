3310b009211a6747ecec17023318b3eecabce959
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-17 11:17:01 +0400

    feat: add content for ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,21 @@
-function ErrorPage() {
-  return <div>ErrorPage</div>;
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'components/Button';
+import Text from 'components/Text';
+import styles from './ErrorPage.module.scss';
+
+function ErrorPage({ errorMessage }: { errorMessage: string }) {
+  const navigate = useNavigate();
+
+  return (
+    <main className={styles.error}>
+      <Text view="title" tag="h2">
+        Something went wrong🥲
+      </Text>
+      <Text view="p-20">{errorMessage}</Text>
+      <Button onClick={() => navigate(-1)}>Go back</Button>
+    </main>
+  );
 }
 
 export default ErrorPage;
