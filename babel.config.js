727dceeeea69fb95e6991730136ca71e2132e6d7
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-24 19:25:51 +0400

    fix: fix typo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 module.exports = (api) => {
-  api.cash.using(() => {
-    process.env.NODE_ENV;
-  });
+  api.cache.using(() => process.env.NODE_ENV);
 
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
