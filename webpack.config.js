65aad74f1f76e70df27510437dc77190bd22ad5b
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-09-24 19:37:33 +0400

    feat: add css, style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.jsx?$/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
